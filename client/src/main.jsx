733aa0516b307d24c42415c73655d9ba0664fcfd
yanis <yanismehalaine@outlook.fr>
2024-02-07 20:56:18 +0100

    feat(Home): created footer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header/index.jsx';
+import Footer from './components/Footer/index.jsx';
 
 import Home from './pages/Home/index.jsx'
 import SignIn from './pages/SignIn/index.jsx'
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<Home />} />
         <Route path='sign-in' element={<SignIn />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>,
 )
