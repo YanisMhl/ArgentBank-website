a5facc0bf52a9c4c2b6734e6851e2424448372f7
yanis <yanismehalaine@outlook.fr>
2024-02-08 15:16:23 +0100

    feat(User): coded User page and account component

--- client/src/pages/User/index.jsx
+++ client/src/pages/User/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Account from "../../components/Account";
 
 const User = () => {
     const navigate = useNavigate();
@@ -13,7 +14,30 @@ const User = () => {
             navigate('/');
         }
     }, [token]);
-    return (<h1>User</h1>);
+    return (
+        <main className="main bg-dark">
+            <div className="header">
+                <h1>Welcome back<br />user !</h1>
+                <button className="edit-button">Edit Name</button>
+            </div>
+            <h2 className="sr-only">Accounts</h2>
+            <Account 
+            title="Argent Bank Checking (x8349)"
+            amount="$2,082.79"
+            description="Available Balance"
+            />
+            <Account 
+            title="Argent Bank Savings (x6712)"
+            amount="$10,928.42"
+            description="Available Balance"
+            />
+            <Account 
+            title="Argent Bank Credit Card (x8349)"
+            amount="$184.30"
+            description="Current Balance"
+            />
+        </main>
+    );
 }
 
 export default User;
